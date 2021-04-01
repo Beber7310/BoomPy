c51d35fe15bcb42cde71daf982809cb47f0115e6
Beber7310 <b.dosda@gmail.com>
2021-04-01 21:26:09 +0200

    add album

--- boomPi.py
+++ boomPi.py
@@ -6,6 +6,8 @@ Created on 27 mars 2021
 from guizero import App,Box,PushButton,os,Window
  
 import platform
+import wndAlbum
+
 
 if __name__ == '__main__':
     pass
@@ -32,46 +34,81 @@ def radio_france_musique():
     os.system("mpc add http://direct.francemusique.fr/live/francemusique-midfi.mp3")
     os.system("mpc play")
  
+ 
+def chicken_open():
+    os.system("")
+    
+def chicken_close():
+    os.system("")
+    
 def window_show_radio():
     appRadio.show()
     if(platform.system()=="Linux"):        
         appRadio.set_full_screen()
     if(platform.system()=="Windows"):
         appRadio.width=800
-        appRadio.height=480
+        appRadio.height=480        
+        
+
+def window_show_chicken():
+    appChicken.show()
+    if(platform.system()=="Linux"):        
+        appChicken.set_full_screen()
+    if(platform.system()=="Windows"):        
+        appChicken.width=800
+        appChicken.height=480
+        
         
 def window_hide_radio():
     appRadio.hide()    
         
 app = App(title="Main")
-appRadio = Window(app, title="Second window")
+appRadio = Window(app)
 appRadio.hide()
+appChicken = Window(app)
+appChicken.hide()
+
 
 if(platform.system()=="Linux"):
     app.set_full_screen()
     appRadio.set_full_screen()
+    appChicken.set_full_screen()
 
 if(platform.system()=="Windows"):
     app.width=800
     app.height=480
     appRadio.width=800
     appRadio.height=480
-
+    appChicken.width=800
+    appChicken.height=480
+    
+wndAlbum.wndAlbumCreate(app)
+    
 #----------------------------------------------------------------------------------------------------------
-
 update_text = PushButton(app, command=window_show_radio,    image = "radio.png")
+update_text = PushButton(app, command=window_show_chicken,    image = "chicken.png")
 
 
 #----------------------------------------------------------------------------------------------------------
 box_radio = Box(appRadio, width="fill", align="top",layout="grid")
 box_home  = Box(appRadio, width="fill", align="bottom")
 
-update_text = PushButton(box_home, command=window_hide_radio,    image = "home.png",         text="Back", align="right")
-
+update_text = PushButton(box_home, command=window_hide_radio,       image = "home.png",         text="Back", align="right")
 update_text = PushButton(box_radio, command=radio_fip,              image = "fip.png",          text="fip",grid=[0,0])
 update_text = PushButton(box_radio, command=radio_france_inter,     image = "franceinter.png",  text="france inter",grid=[1,0])
 update_text = PushButton(box_radio, command=radio_france_culture,   image = "franceculture.png",text="france culture",grid=[2,0])
 update_text = PushButton(box_radio, command=radio_france_musique,   image = "francemusique.png",text="france msique",grid=[3,0])
 
+
+#----------------------------------------------------------------------------------------------------------
+box_chicken_cmd     = Box(appChicken, width="fill", align="top",layout="grid")
+box_chicken_home    = Box(appChicken, width="fill", align="bottom")
+
+update_text = PushButton(box_chicken_home, command=window_hide_radio,   image = "home.png",         text="Back", align="right")
+update_text = PushButton(box_chicken_cmd, command=chicken_open,         image = "open.png",     grid=[0,0])
+update_text = PushButton(box_chicken_cmd, command=chicken_close,        image = "close.png",    grid=[1,0])
+
+
+#----------------------------------------------------------------------------------------------------------
  
 app.display()
---
+++ wndAlbum.py
@@ -0,0 +1,126 @@
+'''
+Created on 28 mars 2021
+
+@author: bdosd
+'''
+from guizero import App,Box,PushButton,os,Window
+from PIL import Image
+ 
+import platform
+import glob
+
+arrayAlbum=[];
+arrayAlbumIdx=0;
+box_album=0
+
+arrayButton=[];
+
+albumPathDesc =""
+basePathDesc = ""
+        
+class record():
+    artiste=""
+    album=""
+    cover=""
+    genre =""
+    songs =[]
+    
+    def loadFromFile(self,filePath):
+        file = open(filePath, 'r')       
+        try:
+            line = file.readline()
+            while line != '':  # The EOF char is an empty string
+                print(line, end='')
+                line = file.readline()                
+                if "<COVER>" in line:                    
+                    self.cover=line.replace("<COVER>","") 
+                    self.cover=self.cover.rstrip()
+                    
+                    print("cover=" + self.cover, end='')                   
+        finally:
+            file.close()
+    
+def nextAlbum():
+    global box_album
+    global arrayAlbumIdx
+    if(arrayAlbumIdx+10 < len(arrayAlbum)):
+        arrayAlbumIdx+=10;
+    UpdateButton(box_album)   
+    
+def prevAlbum():
+    global box_album
+    global arrayAlbumIdx
+    if(arrayAlbumIdx >=10):
+        arrayAlbumIdx-=10
+    if(arrayAlbumIdx<0):
+        arrayAlbumIdx=0
+    UpdateButton(box_album)      
+    
+def wndAlbumCreate(parent):
+    global albumPathDesc
+    global basePathDesc
+    global box_album
+    
+    appAlbum = Window(parent)
+    if(platform.system()=="Linux"):
+        appAlbum.set_full_screen()
+        albumPathDesc = "/mnt/downloads/audion/album/"
+        basePathDesc  = "/mnt/downloads/audion/"
+      
+    if(platform.system()=="Windows"):
+        appAlbum.width=800
+        appAlbum.height=480
+        albumPathDesc ="//OPENMEDIAVAULT/Music/album/"
+        basePathDesc = "//OPENMEDIAVAULT/Music/"
+    
+    box_album = Box(appAlbum, width="fill", align="top",layout="grid")
+    box_home  = Box(appAlbum, width="fill", align="bottom")
+
+    PushButton(box_home,command=prevAlbum, text ="<", align="left")
+    PushButton(box_home,command=nextAlbum, text =">", align="right")
+    
+    #for filename in glob.glob('*.piz'):
+    for filename in glob.glob(os.path.join(albumPathDesc, '*.piz')):
+        albumInst = record();
+        albumInst.loadFromFile(filename)
+        print("I " + basePathDesc , end='')
+        print("I " + albumInst.cover, end='')
+        print("I " + basePathDesc + albumInst.cover, end='')
+       
+        im = Image.open(basePathDesc + albumInst.cover) 
+        newsize = (154,154)
+        im = im.resize(newsize)
+        im.save( basePathDesc + albumInst.cover+"thumb", "JPEG")     
+        
+        arrayAlbum.append(albumInst)
+    
+    UpdateButton(box_album)     
+    
+def UpdateButton(parent):    
+    global albumPathDesc
+    global basePathDesc
+    global arrayAlbum;
+    global arrayAlbumIdx;
+    
+    
+    for x in arrayButton:
+        x.destroy()
+        
+    arrayButton.clear()   
+
+    if(arrayAlbumIdx+0 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+0].cover+"thumb",grid=[0,0]))
+    if(arrayAlbumIdx+1 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+1].cover+"thumb",grid=[1,0]))
+    if(arrayAlbumIdx+2 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+2].cover+"thumb",grid=[2,0]))
+    if(arrayAlbumIdx+3 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+3].cover+"thumb",grid=[3,0]))
+    if(arrayAlbumIdx+4 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+4].cover+"thumb",grid=[4,0]))
+    
+    if(arrayAlbumIdx+5 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+5].cover+"thumb",grid=[0,1]))
+    if(arrayAlbumIdx+6 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+6].cover+"thumb",grid=[1,1]))
+    if(arrayAlbumIdx+7 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+7].cover+"thumb",grid=[2,1]))
+    if(arrayAlbumIdx+8 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+8].cover+"thumb",grid=[3,1]))
+    if(arrayAlbumIdx+9 < len(arrayAlbum)): arrayButton.append(PushButton(parent,  image = basePathDesc+ arrayAlbum[arrayAlbumIdx+9].cover+"thumb",grid=[4,1]))
+    
+    
+    
+    
+
